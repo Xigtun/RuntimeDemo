4738cfa4307077e9a69e8d5b48f13d82909191dc
chenbin <chenbin@chenbins-MacBook-Pro.local>
2016-01-20 20:03:24 +0800

    init

---
+++ RuntimeHUDDemo/UIViewController+HUD.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HUD.h
+//  RuntimeHUDDemo
+//
+//  Created by chenbin on 16/1/19.
+//  Copyright © 2016年 hudDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (HUD)
+
+@end
---
+++ RuntimeHUDDemo/UIViewController+HUD.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+HUD.m
+//  RuntimeHUDDemo
+//
+//  Created by chenbin on 16/1/19.
+//  Copyright © 2016年 hudDemo. All rights reserved.
+//
+
+#import "UIViewController+HUD.h"
+
+@implementation UIViewController (HUD)
+
+@end
